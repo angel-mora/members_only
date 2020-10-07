321ddd54c8724be5fd8b61ab86db236650cb135c
gabrielHilarion <gabrielhhilarion@gmail.com>
2020-10-07 18:14:49 +0100

    linter errors corrected

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
+  def user_is_online(_user)
+    link = if user_signed_in?
+             " #{link_to 'Logout', destroy_user_session_path, method: :delete, class: 'head'}"
 
-   def user_is_online(user)
-     if user_signed_in?  
-      link = " #{link_to "Logout",  destroy_user_session_path, method: :delete, class: "head"}"
-    
-     else
-        link ="#{link_to "Login",  new_user_session_path, class: "head"}"
-      end 
+           else
+             (link_to 'Login', new_user_session_path, class: 'head').to_s
+           end
 
-      link.html_safe
-   end
+    link.html_safe
+  end
 
   private
+
   def post_params
     params.require(:user).permit(:username, :name)
   end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,25 +1,24 @@
 module PostsHelper
-    def display_errors(post)
-       show_error = '' 
-       if post.errors.any?  
+  def display_errors(post)
+    show_error = ''
+    if post.errors.any?
 
-          post.errors.full_messages.each do |message|  
-            show_error += "#{message}. "
-          end  
-        
-       end  
-       return "#{show_error}"
-    end
+      post.errors.full_messages.each do |message|
+        show_error += "#{message}. "
+      end
 
-    def display_name(post)
-        post.user.name if user_signed_in?  
     end
+    show_error.to_s
+  end
 
-    def show_controls_if_poster(user, post)
-        if post.user == current_user 
-          link_to 'Edit', edit_post_path(post) 
-          link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' } 
-        end 
-    end
+  def display_name(post)
+    post.user.name if user_signed_in?
+  end
+
+  def show_controls_if_poster(_user, post)
+    return unless post.user == current_user
+
+    link_to 'Edit', edit_post_path(post)
+    link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }
+  end
 end
-
